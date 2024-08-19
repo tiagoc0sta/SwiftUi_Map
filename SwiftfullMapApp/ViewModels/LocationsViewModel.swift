37e8b461c82731bff029d9e07b557e695b3134ef
tiagoc0sta <tiagodpv@gmail.com>
2024-08-19 14:10:34 -0400

    add next view model

--- SwiftfullMapApp/ViewModels/LocationsViewModel.swift
+++ SwiftfullMapApp/ViewModels/LocationsViewModel.swift
@@ -56,4 +56,26 @@ class LocationsViewModel: ObservableObject {
             showLocationsList = false
         }
     }
+    
+    func nextButtonPressed() {
+        //Get the current index
+        guard let currentIndex = locations.firstIndex(where: {$0 == mapLocation}) else {
+            print("Could not find current indext in locations array! Should never happen.")
+            return
+        }
+        
+        // Check if the currentInex is valid
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            //Next index is NOT valid
+            //Restart from 0
+            guard let firstLocation = locations.first else { return }
+            showNextLocation(location: firstLocation)
+            return
+        }
+        
+        //Next index IS valid
+        let nextLocation = locations[nextIndex]
+        showNextLocation(location: nextLocation)
+    }
 }
