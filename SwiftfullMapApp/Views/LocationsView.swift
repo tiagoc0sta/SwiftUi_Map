e0d23184d2d93d1d437c16d2607e0f0384a989da
tiagoc0sta <tiagodpv@gmail.com>
2024-08-26 00:18:49 -0400

    final version

--- SwiftfullMapApp/Views/LocationsView.swift
+++ SwiftfullMapApp/Views/LocationsView.swift
@@ -12,6 +12,7 @@ import MapKit
 struct LocationsView: View {
   
     @EnvironmentObject private var vm: LocationsViewModel
+    let maxWidthForIpad: CGFloat = 700
     
     var body: some View {
         ZStack {
@@ -20,6 +21,7 @@ struct LocationsView: View {
             VStack (spacing: 0 ){
                 header
                     .padding()
+                    .frame(maxWidth: maxWidthForIpad)
                 Spacer()
                 locationsPreviewStack
             }
@@ -88,6 +90,8 @@ extension LocationsView {
                     LocationPreviewView(location: location)
                         .shadow(color: Color.black.opacity(0.3), radius: 20)
                         .padding()
+                        .frame(maxWidth: maxWidthForIpad)
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
                         .transition(.asymmetric(
                             insertion: .move(edge: .trailing),
                             removal: .move(edge: .leading)))
